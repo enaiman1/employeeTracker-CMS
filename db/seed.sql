199ff8e2d432c51fd6ab1b28c38c7b4450385c6a
Ericcentrik <enaiman1@gmail.com>
2020-01-10 20:54:10 -0500

    added seeds, fixed some bugs in the schema, db is ready to go

---
+++ db/seed.sql
@@ -0,0 +1,34 @@
+use employeesDB;
+
+INSERT INTO department
+    (name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
+
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Jon', 'Doe', 1, NULL),
+    ('John', 'Dinsmore', 2, 1),
+    ('Lilliana', 'Ramos', 3, NULL),
+    ('Eric', 'Naiman', 4, 3),
+    ('Clarisse', 'Mores', 5, NULL),
+    ('Dick', 'Guthrie', 6, 5),
+    ('Massiel', 'Escano', 7, NULL),
+    ('Jorge', 'Alvarez', 8, 7);
+
